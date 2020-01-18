86953794827485eb085fc61294bcb757fddfb086
Michael Jolley <mike@sparcapp.io>
2020-01-18 16:00:54 -0600

    Updating to log payload

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import * as github from '@actions/github'
 //import * as io from '@actions/io'
 
 const run = async (): Promise<void> => {
-  console.log(github.context.eventName)
+  console.log(`Triggered due to: ${github.context.eventName}`)
+
+  console.dir(github.context.payload)
 }
 
 run()
