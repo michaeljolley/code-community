6608ce65924805511958ce77259bdb89e1ebeb32
Michael Jolley <mike@sparcapp.io>
2020-01-18 17:52:06 -0600

    Adding IUser interface

--- dist/contributors.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//import * as io from '@actions/io'
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import * as github from '@actions/github'
 //import * as io from '@actions/io'
 
-import * as issues from './Issues'
+import * as issues from './issues'
 
 const run = async (): Promise<void> => {
   console.log(`Triggered due to: ${github.context.eventName}`)
