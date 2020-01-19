20e1b661af5bb88202ed5f92653921050f4dbbfc
Michael Jolley <mike@sparcapp.io>
2020-01-19 10:04:17 -0600

    Testing reaching filesystem

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,32 @@ import * as github from '@actions/github'
 
 import * as issues from './issues'
 import * as contributors from './contributors'
-import {CONNREFUSED} from 'dns'
+
+var path = require('path')
+var fs = require('fs')
 
 const run = async (): Promise<void> => {
   await contributors.addContributor({}, ['code'])
 
-  switch (github.context.eventName) {
-    case 'issues':
-      issues.processIssue(github.context.payload)
-    default:
-  }
+  fs.readdir(__dirname, function(err: any, files: any) {
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err)
+    }
+    //listing all files using forEach
+    files.forEach(function(file: any) {
+      // Do whatever you want to do with the file
+      console.log(file)
+    })
+  })
+
+  // switch (github.context.eventName) {
+  //   case 'issues':
+  //     issues.processIssue(github.context.payload);
+  //     break;
+  //   default:
+  //     break;
+  //}
 }
 
 run()
